148659529f724a3025dfd983836785c06c8fbcdd
Parthasarathy <parthasarathy@logicsoft.co.in>
2019-08-31 16:23:53 +0530

    [wm] : create channels for API26+

--- app/src/main/java/in/co/logicsoft/workmangersample/UploadWorker.kt
+++ app/src/main/java/in/co/logicsoft/workmangersample/UploadWorker.kt
@@ -1,16 +1,21 @@
 package `in`.co.logicsoft.workmangersample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import kotlin.random.Random
 
 class UploadWorker(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
 
     companion object {
         const val NOTIFICATION_MESSAGE = "NOTIFICATION_MESSAGE"
+        const val CHANNEL_ID = "100"
     }
 
     override fun doWork(): Result {
@@ -20,14 +25,32 @@ class UploadWorker(appContext: Context, workerParams: WorkerParameters) :
     }
 
     private fun triggerNotification(message: String?) {
-        val builder = NotificationCompat.Builder(applicationContext, "100")
+        createNotificationChannel()
+        val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(applicationContext.getString(R.string.app_name))
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
 
         with(NotificationManagerCompat.from(applicationContext)) {
-            notify(0, builder.build())
+            notify(Random.nextInt(), builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = applicationContext.getString(R.string.notification_channel_name)
+            val descriptionText = applicationContext.getString(R.string.notification_channel_desc)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
